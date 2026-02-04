b1839dc4304de2ef9a458c657334ce29e08928f7
manishgounder71-prog <manishgounder71@gmail.com>
2026-02-04 19:55:03 +0530

    chore: increase chunk size warning limit to 1600kB

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
     },
     build: {
         outDir: 'dist',
-        sourcemap: true
+        sourcemap: true,
+        chunkSizeWarningLimit: 1600,
     },
     optimizeDeps: {
         include: ['three', '@react-three/fiber', '@react-three/drei']
